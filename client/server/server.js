8be33cc48644e2e201dfae36e353b978fb94846a
davieu <davisumana@gmail.com>
2018-11-05 15:06:28 -0500

    preparing for deploying to heroku added some code to server

--- client/server/server.js
+++ client/server/server.js
@@ -1,10 +1,42 @@
+/////////////added
+// const {createServer} = require('http');
+////////////
+
 const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-const port = process.env.PORT || 4000;
+// const port = process.env.PORT || 4000;
 const bodyParser = require('body-parser');
 const fs = require('fs');
+
+///////////added
+const compression = require('compression');
+const morgan = require('morgan');
+const path = require('path');
+const normalizePort = port => parseInt(port, 10);
+const port = normalizePort(process.env.PORT || 4000);
+const dev = app.get('env') !== 'production';
+///////////
+
+///////////added
+if (!dev) {
+    app.disable('x-powered-by');
+    app.use(compression());
+    app.use(morgan('common'));
+
+    app.use(express.static(path.resolve(__dirname, 'build')));
+
+    //'*' every request that comes in.
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
+    });
+};
+
+if (dev) {
+    app.use(morgan('dev'))
+};
+//////////
 
 let questions = [];
 let currentCorrectUsers = [];
@@ -94,88 +126,3 @@ io.on('connection', onConnection);
 http.listen(port, () => console.log('listening on port ' + port));
 
 
-// {
-//     "id": 3,
-//     "prompt": "Who was the first woman pilot to fly solo across the Atlantic?:",
-//     "choices": [
-//       "Susan B. Anthony",
-//       "Beyonce",
-//       "Amelia Earhart",
-//       "Hew Keller"
-//     ],
-//     "answer": "Amelia Earhart",
-//     "links": [
-//       {
-//         "href": "www.google.com",
-//         "text": "LMGTFY"
-//       }
-//     ]
-//   },
-//   {
-//     "id": 4,
-//     "prompt": "The Hoover Dam in the United States is built on what river?",
-//     "choices": [
-//       "North Carolina River",
-//       "The Colorado River",
-//       "Yadkin River",
-//       "Hoover River"
-//     ],
-//     "answer": "The Colorado River",
-//     "links": [
-//       {
-//         "href": "www.google.com",
-//         "text": "LMGTFY"
-//       }
-//     ]
-//   },
-//   {
-//     "id": 6,
-//     "prompt": "Steve Jobs, Steve Wozniak, and Ronald Wayne founded what company in 1976?",
-//     "choices": [
-//       "Nintendo",
-//       "Sony",
-//       "Microsoft",
-//       "Apple Computer, Inc."
-//     ],
-//     "answer": "Apple Computer, Inc.",
-//     "links": [
-//       {
-//         "href": "www.google.com",
-//         "text": "LMGTFY"
-//       }
-//     ]
-//   },
-//   {
-//     "id": 7,
-//     "prompt": "How many feet are in a mile?",
-//     "choices": [
-//       "5280",
-//       "328",
-//       "1020",
-//       "About 200"
-//     ],
-//     "answer": "5280",
-//     "links": [
-//       {
-//         "href": "www.google.com",
-//         "text": "LMGTFY"
-//       }
-//     ]
-//   },
-//   {
-//     "id": 9,
-//     "prompt": "All of these are examples of ORMs except?",
-//     "choices": [
-//       "Express",
-//       "Sequelize",
-//       "A custom library taking callbacks",
-//       "Mongoose"
-//     ],
-//     "answer": "Express",
-//     "links": [
-//       {
-//         "href": "www.google.com",
-//         "text": "LMGTFY"
-//       }
-//     ]
-//   },
